b5d3d68e0c803a3537b76bf43280982c30a85b8c
Ben Ross <benarrross@live.com>
2026-09-26 08:59:44 -0700

    Start evolving NodeHandle to just be an index

--- src/pslist/nodehandle.rs
+++ src/pslist/nodehandle.rs
@@ -1,11 +1,12 @@
 use std::sync::{Arc, RwLock};
+use std::cell::*;
 use super::node::*;
 
 
 #[derive(Debug, Clone)]
 pub(super) struct NodeHandle<const K: usize> {
     pub node_debug_id: usize,               // NYI limit this to only exist when running tests
-    node_lock: Arc<RwLock<Node<K>>>         // NYI would this be faster as a Mutex? Or could this be a Box or RefCell?
+    node_lock: RefCell<Box<Node<K>>>         // NYI would this be faster as a Mutex? Or could this be a Box or RefCell?
 }
 
 
@@ -15,16 +16,22 @@ impl<const K: usize> NodeHandle<K> {
 
         NodeHandle {
             node_debug_id: node.debug_id,
-            node_lock: Arc::new(RwLock::new(node))
+            node_lock: RefCell::new(Box::new(node))
         }
     }
 
-    pub fn read_lock(&self) -> std::sync::RwLockReadGuard<'_, Node<K>> {
-        self.node_lock.read().unwrap()
+    pub fn read_lock(&self) -> Ref<'_, Node<K>> {
+        // Rust is an absolutely insane language. I have no idea why you need to use map just to get 
+        // a reference to something on the heap. shoot me now.
+        let borrow = self.node_lock.borrow();
+        Ref::map(borrow, |boxed| boxed.as_ref())
     }
 
-    pub fn write_lock(&self) -> std::sync::RwLockWriteGuard<'_, Node<K>> {
-        self.node_lock.write().unwrap()
+    pub fn write_lock(&self) -> RefMut<'_, Node<K>> {
+        // Rust is an absolutely insane language. I have no idea why you need to use map just to get 
+        // a reference to something on the heap. shoot me now.
+        let borrow = self.node_lock.borrow_mut();
+        RefMut::map(borrow, |boxed| boxed.as_mut())
     }
 }
 
